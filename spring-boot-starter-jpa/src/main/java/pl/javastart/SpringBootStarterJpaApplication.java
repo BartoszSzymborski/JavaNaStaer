6bd99a10f40958e80ba8e96a502bd11ef5f1aaf7
Bartosz <bartoszszymborski@gmail.com>
2018-12-07 15:22:44 +0100

    07.12.2018

--- spring-boot-starter-jpa/src/main/java/pl/javastart/SpringBootStarterJpaApplication.java
+++ spring-boot-starter-jpa/src/main/java/pl/javastart/SpringBootStarterJpaApplication.java
@@ -8,6 +8,8 @@ import pl.javastart.repository.CarRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class SpringBootStarterJpaApplication {
@@ -18,18 +20,53 @@ public class SpringBootStarterJpaApplication {
 
         List<Car> cars = new ArrayList<>();
         cars.add(new Car("A4", "Audi", 49000.0));
+        cars.add(new Car("A5", "Audi", 67000.0));
         cars.add(new Car("Auris", "Toyota", 35000.0));
         cars.add(new Car("Insignia", "Opel", 29500.0));
+        cars.add(new Car("A8", "Audi", 28000.0));
+        cars.add(new Car("Corolla", "Toyota", 31000.0));
+        cars.add(new Car("Vectra", "Opel", 29500.0));
+        cars.add(new Car("Astra", "Opel", 29500.0));
 
         CarRepository carRepo = ctx.getBean(CarRepository.class);
         cars.forEach(carRepo::save); //zapisujemy samochody
+//all
+        System.out.println("All Toyota cars");
+        List<Car> allToyotas = carRepo.findAllByBrand("Toyota");
+        allToyotas.forEach(System.out::println);
+  //first
+        Car opel = carRepo.findFirstByBrand("Opel");
+        System.out.println(opel);
 
-        Car firstCar = carRepo.findById(1L).get(); //pobieramy pierwszy
-        carRepo.delete(firstCar); //usuwamy go
+        //And or
+        System.out.println("Audi for 49000");
+        carRepo.findAllByBrandAndPrice("Audi",49000);
+        System.out.println("Toyota or Ople");
+        carRepo.findAllByBrandOrBrand("Toyota","Opel").forEach(System.out::println);
 
-        //pobieramy i wyświetlamy pozostałe
-        carRepo.findAll().forEach(System.out::println);
+        System.out.println("Cars with brand starting with A");
+        carRepo.findAllByBrandLike("A%").forEach(System.out::println);
+
+        System.out.println("Cars with name ending with 'tra' ");
+        carRepo.findAllByNameEndingWith("tra").forEach(System.out::println);
+
+        System.out.println("Cars cheaper than 30000");
+        carRepo.findAllByPriceLessThan(30000).forEach(System.out::println);
+
+        System.out.println("Cars with price between 30-40k");
+        carRepo.findAllByPriceBetween(30000,40000).forEach(System.out::println);
+
+        System.out.println("All Audi cars, price ascending");
+        carRepo.findAllByBrandOrderByPriceAsc("Audi").forEach(System.out::println);
+
+        System.out.println("Cars with brand");
+        carRepo.findAllByBrandNotNull().forEach(System.out::println);
+
+        System.out.println("Car name in {Insygnia, Corolla} ");
+        List<String>carNames = Stream.of("Insygnia","Corolla").collect(Collectors.toList());
+        carRepo.findAllByNameIn(carNames).forEach(System.out::println);
 
         ctx.close();
     }
     }
+
--- spring-boot-starter-jpa/src/main/java/pl/javastart/repository/CarRepository.java
+++ spring-boot-starter-jpa/src/main/java/pl/javastart/repository/CarRepository.java
@@ -1,9 +1,40 @@
 package pl.javastart.repository;
 
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pl.javastart.model.Car;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.ListResourceBundle;
+
 @Repository
 public interface CarRepository extends CrudRepository<Car, Long> {
+
+    List<Car>findAllByBrand(String brand);
+
+    Car findFirstByBrand(String brand);
+
+    Car findFirstByPrice(double price);
+
+    List<Car> findFirst3ByBrand(String brand);
+
+    List<Car>findAllByBrandAndPrice(String brand, double price);
+
+    List<Car>findAllByBrandOrBrand(String brand1, String bran2);
+
+    List<Car>findAllByBrandLike(String pattern);
+
+    List<Car>findAllByNameEndingWith(String pattern);
+
+    List<Car>findAllByPriceLessThan(double price);
+
+    List<Car>findAllByPriceBetween(double low, double high);
+
+    List<Car>findAllByBrandOrderByPriceAsc(String brand);
+
+    List<Car>findAllByBrandNotNull();
+
+    List<Car>findAllByNameIn(Collection<String>names);
 }
