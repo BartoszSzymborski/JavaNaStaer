2c13b0bd1cf3d623d12c6c1f37d82ea91e63bf79
Bartosz <bartoszszymborski@gmail.com>
2018-12-10 11:23:15 +0100

    10.12.2018

---
+++ spring-mvc-calculator/src/main/java/pl/javastart/SpringMvcCalculatorApplication.java
@@ -0,0 +1,12 @@
+package pl.javastart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcCalculatorApplication.class, args);
+    }
+}
