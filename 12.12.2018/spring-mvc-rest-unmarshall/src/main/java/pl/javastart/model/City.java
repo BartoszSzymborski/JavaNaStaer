d607ba2ef4584bd864f721c97c6463fff4ebc414
Bartosz <bartoszszymborski@gmail.com>
2018-12-12 14:28:53 +0100

    12.12.2018

---
+++ 12.12.2018/spring-mvc-rest-unmarshall/src/main/java/pl/javastart/model/City.java
@@ -0,0 +1,48 @@
+package pl.javastart.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Objects;
+
+@XmlRootElement
+public class City {
+    private String name;
+    private int population;
+
+    public City() {
+    }
+
+    public City(String name, int population) {
+        this.name = name;
+        this.population = population;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        City city = (City) o;
+        return population == city.population &&
+                Objects.equals(name, city.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, population);
+    }
+}
