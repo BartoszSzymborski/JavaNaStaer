550f21131a133c62ad40cb98aa6c57dc53197f0e
Bartosz <bartoszszymborski@gmail.com>
2018-12-13 16:20:30 +0100

    13.12.2018

---
+++ 13.13.2018/spring-valiator-mvc/src/main/java/pl/javastart/SpringValiatorMvcApplication.java
@@ -0,0 +1,14 @@
+package pl.javastart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringValiatorMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringValiatorMvcApplication.class, args);
+    }
+
+}
+
