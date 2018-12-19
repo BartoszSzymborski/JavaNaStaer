d63166530f5a109ea3cdfe5ed7c987204525d192
Bartosz <bartoszszymborski@gmail.com>
2018-12-19 14:30:25 +0100

    19.12.2018

---
+++ 19.12.2018/spring-angular-http3/src/main/java/pl/javastart/SpringAngularHttp3Application.java
@@ -0,0 +1,14 @@
+package pl.javastart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularHttp3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAngularHttp3Application.class, args);
+    }
+
+}
+
