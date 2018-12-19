f95ca9bee8f7548e840f9d96d9732206f725c378
Bartosz <bartoszszymborski@gmail.com>
2018-12-19 14:31:47 +0100

    19.12.2018

---
+++ 19.12.2018/spring-angular-routing1/src/main/java/pl/javastart/respository/BookRepository.java
@@ -0,0 +1,7 @@
+package pl.javastart.respository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.javastart.model.Book;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
