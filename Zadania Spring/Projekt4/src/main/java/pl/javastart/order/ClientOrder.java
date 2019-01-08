a691a79690ce461e520e5922022a1ae54067c53e
Bartosz <bartoszszymborski@gmail.com>
2019-01-08 17:48:47 +0100

    08.01.2019

---
+++ Zadania Spring/Projekt4/src/main/java/pl/javastart/order/ClientOrder.java
@@ -0,0 +1,32 @@
+package pl.javastart.order;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+import pl.javastart.item.Item;
+
+@Component
+@SessionScope
+public class ClientOrder {
+
+    private Order order;
+
+    public ClientOrder(){
+        clear();
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+    void clear(){
+        order = new Order();
+        order.setStatus(OrderStatus.NEW);
+    }
+
+    public void add(Item item) {
+        order.getItems().add(item);
+    }
+}
